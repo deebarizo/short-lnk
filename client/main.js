122df854f7f66c23e3db275b5d7677657bfee0cd
Dee Barizo <deebarizo@gmail.com>
2018-01-24 18:06:47 -0600

    Add routes

--- client/main.js
+++ client/main.js
@@ -1,10 +1,25 @@
-import {Meteor} from 'meteor/meteor';
+import { Meteor } from 'meteor/meteor';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'; 
+import createBrowserHistory from 'history/createBrowserHistory';
 
+import Login from './../imports/ui/Login';
 import Signup from './../imports/ui/Signup';
 import Link from './../imports/ui/Link';
+import NotFound from './../imports/ui/NotFound';
+
+const routes = (
+    <Router>
+        <Switch>
+        	<Route exact path="/" component={Login} />
+            <Route exact path="/signup" component={Signup} />
+            <Route exact path="/links" component={Link} />
+            <Route component={NotFound} />
+        </Switch>
+    </Router>
+);
 
 Meteor.startup(() => {
-	ReactDOM.render(<Link />, document.getElementById('app'));	
+	ReactDOM.render(routes, document.getElementById('app'));	
 });
