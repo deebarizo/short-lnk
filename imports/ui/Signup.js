eaeed09ad6f554fc90a09439f859801216d98916
deebarizo <deebarizo@gmail.com>
2018-02-07 20:07:44 -0600

    Add state

--- imports/ui/Signup.js
+++ imports/ui/Signup.js
@@ -2,12 +2,33 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default class Signup extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			error: ""
+		};
+	}
+
+	onSubmit(e) {
+		e.preventDefault();
+
+		this.setState({
+			error: "Something went wrong."
+		});		
+	}
+
 	render() {
 		return (
 			<div>
 				<h1>Join Short Lnk</h1>
 
-				<p>Signup form here</p>
+				{this.state.error ? <p>{this.state.error}</p> : undefined}
+
+				<form onSubmit={this.onSubmit.bind(this)}>
+					<input type="email" name="email" placeholder="Email" />
+					<input type="password" name="password" placeholder="Password" />
+					<button>Create Account</button>
+				</form>
 
 				<Link to="/">Already have an account?</Link>
 			</div>
