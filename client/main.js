764c7381f4275886a540288764a10661f37da15b
Dee Barizo <deebarizo@gmail.com>
2018-02-08 02:26:14 -0600

    Add history.push to use with React Router 4

--- client/main.js
+++ client/main.js
@@ -1,23 +1,52 @@
 import { Meteor } from 'meteor/meteor';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Router, Switch, Route } from 'react-router-dom';
+import { Tracker } from 'meteor/tracker';
+import history from './../imports/history';
 
 import Login from './../imports/ui/Login';
 import Signup from './../imports/ui/Signup';
 import Link from './../imports/ui/Link';
 import NotFound from './../imports/ui/NotFound';
 
+const unauthenticatedPages = ['/', '/signup'];
+const authenticatedPages = ['/links'];
+let isUnauthenticatedPage = true;
+let isAuthenticatedPage = false;
+
 const routes = (
-    <Router>
-        <Switch>
-        	<Route exact path="/" component={Login} />
-            <Route exact path="/signup" component={Signup} />
-            <Route exact path="/links" component={Link} />
-            <Route component={NotFound} />
-        </Switch>
+    <Router history={history}>
+    	<div>
+	        <Switch>
+	        	<Route exact path="/" component={Login} />
+	            <Route path="/signup" component={Signup} />
+	            <Route path="/links" component={Link} />
+	            <Route component={NotFound} />
+	        </Switch>
+        </div>
     </Router>
 );
+
+Tracker.autorun(() => {
+	const isAuthenticated = !!Meteor.userId();
+
+	console.log('isAuthenticated', isAuthenticated);
+
+    const pathname = history.location.pathname;
+    isUnauthenticatedPage = unauthenticatedPages.includes(pathname);
+    isAuthenticatedPage = authenticatedPages.includes(pathname);
+	
+	if (isAuthenticated) {
+		if (isUnauthenticatedPage) {
+			console.log('push to /links');
+			history.push('/links');
+		}
+	} else if (isAuthenticatedPage) {
+		console.log('push to /');
+		history.push('/');
+	}
+});
 
 Meteor.startup(() => {
 	ReactDOM.render(routes, document.getElementById('app'));	
---
+++ imports/history.js
@@ -0,0 +1,5 @@
+// https://stackoverflow.com/a/42320249
+
+import createHistory from 'history/createBrowserHistory';
+
+export default createHistory();
