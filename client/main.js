b6041454a615d3c68fd0ab81c6feaf6af929366f
Dee Barizo <deebarizo@gmail.com>
2018-03-01 12:17:25 +0800

    Work on links

--- client/main.js
+++ client/main.js
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom';
 import { Tracker } from 'meteor/tracker';
 
 import { routes, onAuthChange } from '../imports/routes/routes';
+import { Links } from '../imports/api/links';
 
 Tracker.autorun(() => {
 	const isAuthenticated = !!Meteor.userId();
 	onAuthChange(isAuthenticated);
 
 	console.log('isAuthenticated', isAuthenticated);
+});
+
+Tracker.autorun(() => {
+	const links = Links.find().fetch();
+	console.log('New links', links);
 });
 
 Meteor.startup(() => {
---
+++ imports/ui/LinksList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class LinksList extends React.Component {
+	render() {
+		return (
+			<div>
+				<p>Links List</p>
+
+				
+			</div>
+		);
+	}
+}
