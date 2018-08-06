4a143b6cf30bf4643ee10f3a8dba73f0865ac1bb
Dee Barizo <deebarizo@gmail.com>
2018-08-05 20:50:20 -0500

    Create LinksListItem component

---
+++ imports/ui/LinksListItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class LinksListItem extends React.Component {
+	render() {
+		return (
+			<div>
+				<p>{this.props.url}</p>
+				<p>{this.props.shortUrl}</p>
+			</div>
+		);
+	}
+}
