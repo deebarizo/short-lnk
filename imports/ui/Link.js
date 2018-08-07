75c9198daa6d03af48c11f18847d042cc53a91ac
Dee Barizo <deebarizo@gmail.com>
2018-08-06 20:50:22 -0500

    Misc

--- imports/ui/Link.js
+++ imports/ui/Link.js
@@ -4,11 +4,13 @@ import { Links } from '../api/links';
 import LinksList from '../ui/LinksList';
 import PrivateHeader from '../ui/PrivateHeader';
 import AddLink from '../ui/AddLink';
+import LinksListFilters from '../ui/LinksListFilters';
 
 export default () => {
 	return (
 		<div>
 			<PrivateHeader title="Your Links" />
+			<LinksListFilters />
 			<LinksList />
 			<AddLink />
 		</div>
